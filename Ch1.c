eb2b7eec6cf477759ca0e97af944b426c6a9889b
Blain Wu <896511142@qq.com>
2020-10-08 16:35:46 +0800

    update

--- Ch1.c
+++ Ch1.c
@@ -2,11 +2,70 @@
 
 //1.2 Variables and arithmetic expression
 void Fahrenheit_Celsius(){
+    float fahr,celsius;
+    int lower,upper,step;
 
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while(fahr<=upper){
+        celsius = 5*(fahr-32)/9;
+        printf("%3.0f\t%6.1f\n",fahr,celsius);
+        fahr = fahr + step;
+    }
 }
 
+void Fahrenheit_Celsius_for(){
+    int fahr;
+    for(fahr = 0 ;fahr<=300;fahr+=20)
+        printf("%3d %6.1f\n",fahr,(5.0/9.0)*(fahr-32));
+}
+
+void char_io(){
+    int c ;
+    c = getchar();
+    while(c != EOF){
+        putchar(c);
+        c = getchar();
+    }
+}
+
+void char_io_2(){
+    int c ;
+    while((c = getchar()) != EOF)
+        putchar(c);
+}
+
+void verify(){
+    int c ;
+    while(c = getchar()!=EOF)
+        printf("%d",EOF);
+}
+
+void char_counter(){
+    long nc ;
+    nc = 0;
+    while (getchar() != EOF )
+        ++nc;
+    printf("%ld",nc);
+}
 
 int main() {
-    printf("Hello, World!\n");
+    Fahrenheit_Celsius();
+
+    printf("-------------------------------\n");
+
+    Fahrenheit_Celsius_for();
+
+    printf("-------------------------------\n");
+
+    //char_io_2();
+
+    //verify();
+
+    //char_counter();
+
     return 0;
 }
--- README.md
+++ README.md
@@ -3,7 +3,16 @@
 
 ***
 
-#Chapter1
+# Chapter1
 1. `\n` is C notation for the *newline character*.  
-2. In C ,all avariables must be declared before they are used.  
-3. 
+2. In C ,all variables must be declared before they are used.  
+3. Recommended that writing only one statement per line, and using 
+blanks around operators to clarify grouping.  
+4. The braces `{ }`position is less important.  
+5. `#define` line defines a symbolic name or symbolic constant to be
+a particular string of characters.  
+6. `putchar()` and `getchar()` are both operators of int data which is 
+ascii code.  
+7. `EOF` means *end of file* ,whose value si `-1` .  
+
+
